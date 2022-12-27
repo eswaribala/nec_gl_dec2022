715b14e46df4413f63550ce480034642fedce4aa
eswaribala <parameswaribala@gmail.com>
2022-12-27 13:52:39 +0530

    updated

--- day1/CLITest.go
+++ day1/CLITest.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -10,18 +11,36 @@ func main() {
 	fmt.Println("Testing....")
 	fmt.Println(len(args))
 	if len(args) == 4 {
-		result := configureServer(args)
-		for index, value := range result {
-			fmt.Println(index, value)
+		result, err := configureServer(args)
+		if err != nil {
+			for index, value := range result {
+				fmt.Println(index, value)
+			}
+		} else {
+			fmt.Println(err)
 		}
 	}
 }
 
-func configureServer(serverProperties []string) []string {
+func configureServer(serverProperties []string) ([]string, error) {
 	//create another array
 	serverProps := make([]string, 4)
+	//traditional loop
+	for i := 1; i < len(serverProperties); i++ {
+		fmt.Println(serverProperties[i])
+	}
+
+	port, err := strconv.Atoi(serverProperties[1])
+
+	if err != nil {
+		return nil, err
+	}
+	if port < 1027 {
+		return nil, err
+	}
+
 	for index, value := range serverProperties {
 		serverProps[index] = value
 	}
-	return serverProps
+	return serverProps, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module necdec2022
 
 go 1.15
+
+require (
+	github.com/appliedgocode/goman v0.3.0 // indirect
+	github.com/go-delve/delve v1.20.1 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+)
