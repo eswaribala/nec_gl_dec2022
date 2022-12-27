4227c819e5d7ed4029ea0bec14259f0fc004efd1
eswaribala <parameswaribala@gmail.com>
2022-12-27 13:59:58 +0530

    updated

--- day1/CLITest.go
+++ day1/CLITest.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println(len(args))
 	if len(args) == 4 {
 		result, err := configureServer(args)
-		if err != nil {
+		if err == nil {
 			for index, value := range result {
 				fmt.Println(index, value)
 			}
@@ -26,9 +26,9 @@ func configureServer(serverProperties []string) ([]string, error) {
 	//create another array
 	serverProps := make([]string, 4)
 	//traditional loop
-	for i := 1; i < len(serverProperties); i++ {
+	/*for i := 1; i < len(serverProperties); i++ {
 		fmt.Println(serverProperties[i])
-	}
+	}*/
 
 	port, err := strconv.Atoi(serverProperties[1])
 
