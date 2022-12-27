d3bdb35825a35b66f78b42f3f587695e13346140
eswaribala <parameswaribala@gmail.com>
2022-12-27 15:42:00 +0530

    updated

--- day2/models/User.go
+++ day2/models/User.go
@@ -1,5 +1,8 @@
 package models
 
+//sharable attributes should always follow
+//every words first letter in upper case
+
 type User struct {
 	UserId     int32
 	FirstName  string
--- day2/utility/UserApp.go
+++ day2/utility/UserApp.go
@@ -8,9 +8,10 @@ import (
 func main() {
 	//create user instance
 	var user models.User
-	user = models.User{3486586, "Parameswari",
-		"Bala", "",
-		models.Date{2, 12, 1970}}
-	fmt.Printf("User Data%v\n", user)
+	user = models.User{UserId: 3486586, FirstName: "Parameswari",
+		LastName: "Bala", MiddleName: "",
+		DOB: models.Date{Day: 2, Month: 12, Year: 1970}}
+
+	fmt.Printf("User Data%+v\n", user)
 
 }
