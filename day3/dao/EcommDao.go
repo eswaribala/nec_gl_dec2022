bf2c403313afd44360e89e5ef1c5226639d1756c
eswaribala <parameswaribala@gmail.com>
2022-12-28 15:44:23 +0530

    updated

--- day3/dao/EcommDao.go
+++ day3/dao/EcommDao.go
@@ -2,6 +2,8 @@ package dao
 
 import (
 	"database/sql"
+	//blank import -> init()
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
 
@@ -15,4 +17,16 @@ func DBHelper() *sql.DB {
 		log.Fatal(err)
 	}
 	return db
+}
+func CreateProduct(ProductId int32, Name string, Cost int64, DOP string) (int64, error) {
+
+	db := DBHelper()
+	defer db.Close()
+	queryString := "Insert into Product (ProductId,Name,Cost,DOP) values(?,?,?)"
+	result, err := db.Exec(queryString, ProductId, Name, Cost, DOP)
+	if err != nil {
+		log.Fatal("Error occurred while saving...", err)
+	}
+	return result.RowsAffected()
+
 }
--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module necdec2022
 
 go 1.15
 
-require (
-	github.com/appliedgocode/goman v0.3.0 // indirect
-	github.com/go-delve/delve v1.20.1 // indirect
-	golang.org/x/crypto v0.4.0 // indirect
-)
+require github.com/go-sql-driver/mysql v1.7.0
