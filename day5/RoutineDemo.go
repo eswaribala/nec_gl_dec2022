4e5ad919b0af7aa2bfd07d5f21866e7710cc2913
eswaribala <parameswaribala@gmail.com>
2023-01-04 15:07:36 +0530

    updated

--- day5/RoutineDemo.go
+++ day5/RoutineDemo.go
@@ -12,7 +12,7 @@ func goaccessLink(link string, pChannel chan string) {
 		fmt.Println(err)
 	} else {
 		fmt.Printf("link:%s,status code:%d\n", link, response.StatusCode)
-		pChannel <- "link up and running"
+		pChannel <- link + ":link up and running"
 	}
 
 }
@@ -24,15 +24,15 @@ func main() {
 		"http://facebook.com",
 		"http://golang.com",
 	}
-
+	//create channel
 	channel := make(chan string)
 	for _, link := range links {
 		//no communication between main and routines
 		go goaccessLink(link, channel)
 	}
 
-	for _, link := range links {
-		fmt.Printf("Link:%s,response:%s\n", link, <-channel)
+	for _ = range links {
+		fmt.Printf("response:%s\n", <-channel)
 	}
 	fmt.Printf("Number of routines running=%d\n", runtime.NumGoroutine())
 }
