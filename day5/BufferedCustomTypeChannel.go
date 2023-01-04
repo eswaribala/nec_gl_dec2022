eb7237cba94c939ad7b596f93fec3f0d882a0b13
eswaribala <parameswaribala@gmail.com>
2023-01-04 16:39:06 +0530

    updated

--- day5/BufferedCustomTypeChannel.go
+++ day5/BufferedCustomTypeChannel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"necdec2022/day5/models"
 	"time"
 )
@@ -9,12 +10,10 @@ import (
 func ProductWrite(ch chan models.Product) {
 
 	for i := 0; i < 10; i++ {
-		ch <- models.Product{ProductId: 237867, Name: "Door", Cost: 20000,
+		ch <- models.Product{ProductId: rand.Int31n(10000), Name: "Door", Cost: rand.Int63n(10000),
 			DOP: models.Date{Day: 11, Month: 12, Year: 2022}}
 		fmt.Println("Successfully Wrote", i, "to channel")
-		/*if i == 3 {
-			ch = make(chan int, 10)
-		}*/
+
 	}
 	close(ch)
 }
---
+++ day5/ChannelSelectDemo.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	//Economic Trading
+	et := make(chan string)
+	//Non Economic Trading
+	net := make(chan string)
+	//Delta calculation
+	dc := make(chan string)
+
+	//anonymous function
+	go func(message string, dc chan string) {
+
+		time.Sleep(1000)
+		if strings.ContainsAny(message, "Delta") {
+			dc <- "Delta Ready"
+		} else {
+			dc <- "No Delta Calculations"
+		}
+
+	}("ComputeDelta", dc)
+
+	go ecoTrading("Compute ET", et)
+	go nonEcoTrading("Compute Leg1 and Leg2", net)
+
+	//select channel receiver
+	func() {
+
+		select {
+		case msg1 := <-et:
+			fmt.Println(msg1)
+		case msg2 := <-net:
+			fmt.Println(msg2)
+		case msg3 := <-dc:
+			fmt.Println(msg3)
+		}
+
+	}()
+
+	//	var decision string
+	//fmt.Println("Do you want to close Trading")
+	//fmt.Scanln(&decision)
+
+}
+
+func ecoTrading(message string, et chan string) {
+
+	time.Sleep(2000)
+	if len(message) == 0 {
+		et <- "Message cannot be empty"
+	} else {
+		et <- "Eco Trading Done"
+	}
+
+}
+
+func nonEcoTrading(message string, net chan string) {
+
+	time.Sleep(3000)
+	if strings.HasPrefix(message, "Leg1") {
+		net <- "Non Economic Trading Done..."
+	} else {
+		net <- "Not Done..."
+	}
+
+}
