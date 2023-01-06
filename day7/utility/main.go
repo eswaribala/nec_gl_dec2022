90a78b9a5a30daea4dcc28bc25058e879f9e48e5
eswaribala <parameswaribala@gmail.com>
2023-01-06 14:58:02 +0530

    updated

--- day7/utility/main.go
+++ day7/utility/main.go
@@ -1,5 +1,15 @@
 package main
 
+// @title Customer API
+// @version 1.0
+// @description This is a sample service for managing Customer
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email parameswaribala@gmail.com
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:6064
+// @BasePath /
 func main() {
 
 }
