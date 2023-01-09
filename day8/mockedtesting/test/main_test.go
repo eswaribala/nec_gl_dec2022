31a84c3c0ec56b560d1c62bf0428479c8e5551d2
eswaribala <parameswaribala@gmail.com>
2023-01-09 16:59:00 +0530

    updated

--- day8/mockedtesting/test/main_test.go
+++ day8/mockedtesting/test/main_test.go
@@ -23,18 +23,12 @@ func TestGenearteOTP(t *testing.T) {
 	assert := assert2.New(t)
 	// get our mockRand
 	m := newMockOTP()
-	// specify our return value. Since the code in divByRand
-	// passes 10 into randomInt, we pass 10 in as the argument
-	// to go with randomInt, and specify that we want the
-	// method to return 6.
+
 	m.On("GetOTP", 10000).Return(int(rand.Int31n(10)))
 
-	// now run divByRand and assert that we got back the
-	// return value we expected, just like in a Go test that
-	// doesn't use Testify Mock.
 	quotient := src.GenerateOTP(10000, m)
 
-	// check that randomInt was called with the number 10;
+	// check that GetOTP was called with the number 10000;
 	// if not then the test fails
 	m.AssertCalled(t, "GetOTP", 10000)
 	assert.True(quotient > 0)
